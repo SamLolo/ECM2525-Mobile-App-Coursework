4da284258b0c0845f346f0bd5c1a05d95c84f89b
SamLolo <sam.e.townley@icloud.com>
2024-02-23 18:18:42 +0000

    Added listener to get the contents of the autocompleted text view once station selected

--- app/src/main/java/com/example/railinfo/SelectStationActivity.java
+++ app/src/main/java/com/example/railinfo/SelectStationActivity.java
@@ -4,8 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -65,7 +70,16 @@ public class SelectStationActivity extends AppCompatActivity {
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, names_list);
 
-        AutoCompleteTextView textView = (AutoCompleteTextView)findViewById(R.id.enter_station_autocomplete);
+        AutoCompleteTextView textView = (AutoCompleteTextView) findViewById(R.id.enter_station_autocomplete);
         textView.setAdapter(adapter);
+        textView.setOnItemClickListener(new AutoCompleteListener());
+    }
+
+    static class AutoCompleteListener implements AdapterView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            TextView item = (TextView)view;
+            System.out.println(item.getText());
+        }
     }
 }
