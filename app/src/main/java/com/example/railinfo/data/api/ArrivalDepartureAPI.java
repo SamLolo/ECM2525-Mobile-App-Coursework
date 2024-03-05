bc427c139a889d10831671a998f0b23c011dde7f
SamLolo <sam.e.townley@icloud.com>
2024-03-05 20:16:02 +0000

    Added comments to the API tasks

--- app/src/main/java/com/example/railinfo/data/api/ArrivalDepartureAPI.java
+++ app/src/main/java/com/example/railinfo/data/api/ArrivalDepartureAPI.java
@@ -17,17 +17,48 @@ import java.util.concurrent.Callable;
 
 import javax.net.ssl.HttpsURLConnection;
 
+
+/**
+ * A callable task for fetching arrivals and departures from the National Rail API.
+ * Designed as callable so it can be executed off the main thread by an activity.
+ * <p>
+ * Uses the Live Arrival and Departure Boards endpoint from the National Rail's Rail Data
+ * Marketplace. This endpoint returns information (in json) about the next 10 services to arrive
+ * or depart the station specified using the CRS code. For more information, visit
+ * <a href="https://raildata.org.uk/dataProduct/P-7c866984-8a7b-4272-a6f8-00b4aaf821fa/overview">https://raildata.org.uk/</a>
+ *
+ * @author Sam Townley
+ * @version 1.0
+ */
 public class ArrivalDepartureAPI implements Callable<JSONObject> {
+
+    /** The Computer-Reservation System (CRS) code of the station to get services for. */
     private final String crs;
 
+    /**
+     * Constructs a new instance of the ArrivalsDepartureAPI callable based on the station
+     * crs code passed in.
+     *
+     * @param crs The crs code of the station to load data for.
+     */
     public ArrivalDepartureAPI (String crs) {
         this.crs = crs;
     }
 
+    /**
+     * Calls the endpoint with the crs code specified when constructing the class.
+     * <p>
+     * Uses the ArrDepAPIKey specified in the BuildConfig and gradle.properties to hide this
+     * key from Github. The API call is to a standard REST interface, using the GET method.
+     *
+     * @return A JSONObject containing the parsed data returned from the API.
+     */
     @Override
     public JSONObject call() {
+        // Create a string builder to store the response as it's read from the input stream
         StringBuilder response = new StringBuilder();
         try {
+
             // Create HTTPS connection to Rail Data API
             String uri = String.format(Locale.US, "https://api1.raildata.org.uk/1010-live-arrival-and-departure-boards-arr-and-dep/LDBWS/api/20220120/GetArrDepBoardWithDetails/%s", crs);
             URL url = new URL(uri);
@@ -36,6 +67,7 @@ public class ArrivalDepartureAPI implements Callable<JSONObject> {
             connection.setRequestProperty("x-apikey", BuildConfig.ArrDepAPIKey);
             connection.connect();
 
+            // Check if the connection was successful
             if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
                 System.out.println(connection.getResponseMessage());
                 return null;
@@ -47,6 +79,8 @@ public class ArrivalDepartureAPI implements Callable<JSONObject> {
             while ((line = reader.readLine()) != null) {
                 response.append(line).append("\n");
             }
+
+        // Handle IOException that could occur during HTTPS connection
         } catch (IOException e) {
             e.printStackTrace();
             return null;
@@ -56,11 +90,15 @@ public class ArrivalDepartureAPI implements Callable<JSONObject> {
         JSONObject json;
         try {
             json = new JSONObject(response.toString());
+
+            // Add a timestamp the data was fetched to each service within the result
             DateFormat df = DateFormat.getTimeInstance();
             for (int i=0; i < json.getJSONArray("trainServices").length(); i++) {
                 json.getJSONArray("trainServices").getJSONObject(i).put("timestamp", df.format(Calendar.getInstance().getTime()));
             }
             return json;
+
+        // Handle any JSONExceptions that occur whilst parsing the string
         } catch (JSONException e) {
             e.printStackTrace();
             return null;
--- app/src/main/java/com/example/railinfo/data/api/StationInfoAPI.java
+++ app/src/main/java/com/example/railinfo/data/api/StationInfoAPI.java
@@ -15,15 +15,44 @@ import java.util.concurrent.Callable;
 
 import javax.net.ssl.HttpsURLConnection;
 
+/**
+ * A callable task for fetching station information from the National Rail API.
+ * Designed as callable so it can be executed off the main thread by an activity.
+ * <p>
+ * Uses the Knowledgebase Stations data feed endpoint from the National Rail's Rail Data
+ * Marketplace. This endpoint returns information (in XML) about the station specified
+ * using the CRS code. For more information, visit
+ * <a href="https://raildata.org.uk/dataProduct/P-88ffe920-471c-4fd9-8e0d-95d5b9b7a257/overview">https://raildata.org.uk/</a>
+ *
+ * @author Sam Townley
+ * @version 1.0
+ */
 public class StationInfoAPI implements Callable<StationData> {
+
+    /** The Computer-Reservation System (CRS) code of the station to get information for. */
     private final String crs;
+
+    /** The instance of the XMLParser to use when reading the content returned from the API */
     private final StationsXMLParser parser;
 
+    /**
+     * Constructs a new callable API task for retrieving information about a specified station.
+     *
+     * @param crs The CRS code of the station to fetch information about.
+     */
     public StationInfoAPI(String crs) {
         this.crs = crs;
         parser = new StationsXMLParser();
     }
 
+    /**
+     * Calls the endpoint with the crs code specified when constructing the class.
+     * <p>
+     * Uses the StationsAPIKey specified in the BuildConfig and gradle.properties to hide this
+     * key from Github. The API call is to a standard REST interface, using the GET method.
+     *
+     * @return A JSONObject containing the parsed data returned from the API.
+     */
     @Override
     public StationData call() {
         try {
@@ -35,15 +64,19 @@ public class StationInfoAPI implements Callable<StationData> {
             connection.setRequestProperty("x-apikey", BuildConfig.StationsAPIKey);
             connection.connect();
 
+            // Check if the connection was successful
             if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
                 System.out.println(connection.getResponseMessage());
                 return null;
             }
 
-            // Read XML content from API into String
+            // Get a BufferedInputStream and pass this to the parser to read, returning the
+            // resulting StationData object.
             BufferedInputStream input = new BufferedInputStream(connection.getInputStream());
             return parser.parse(input);
 
+        // Handle any IOExceptions that occur whilst carrying out the HTTPS request and
+        // handle any XmlPullParserExceptions that occur whilst parsing the XML.
         } catch (IOException | XmlPullParserException e) {
             e.printStackTrace();
         }
